7c0325878ce0da056c22497edf0e781853a30b8d
aoonsyed <60184690+aoonsyed@users.noreply.github.com>
2025-04-23 11:56:08 +0500

    Initial Commit

---
+++ game_backend/game_app/admin.py
@@ -0,0 +1,15 @@
+# from django.contrib import admin
+#
+# # Register your models here.
+
+#
+# Conditional statement
+
+a= 44
+b =55
+
+if b > a:
+    print("B is greater than a")
+#
+# if condition:
+#     pri
