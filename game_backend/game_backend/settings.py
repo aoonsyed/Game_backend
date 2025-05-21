b388c898a47f8ad1a4193dd679e45892234e220e
Ahmad <168859609+ahmad0iq@users.noreply.github.com>
2025-05-21 11:07:00 +0500

    Add cors headers.

--- game_backend/game_backend/settings.py
+++ game_backend/game_backend/settings.py
@@ -40,9 +40,11 @@ INSTALLED_APPS = [
     "game_app",
     "rest_framework",
     "drf_yasg",
+    'corsheaders',
 ]
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -51,6 +53,8 @@ MIDDLEWARE = [
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
+
+CORS_ALLOW_ALL_ORIGINS = True
 
 ROOT_URLCONF = 'game_backend.urls'
 
