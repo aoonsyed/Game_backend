461006f24a835fbe4db074aeba2da7e9bc01ca4e
aoonsyed <60184690+aoonsyed@users.noreply.github.com>
2025-07-15 01:16:56 +0500

    Update celery.py

--- game_backend/game_backend/celery.py
+++ game_backend/game_backend/celery.py
@@ -1,8 +1,17 @@
 import os
 from celery import Celery
+from celery.schedules import crontab
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'game_backend.settings')
 
 app = Celery('game_backend')
 app.config_from_object('django.conf:settings', namespace='CELERY')
 app.autodiscover_tasks()
+
+
+app.conf.beat_schedule = {
+    'reset-leaderboard-every-sunday-midnight-est': {
+        'task': 'game_app.tasks.reset_leaderboard',
+        'schedule': crontab(minute=0, hour=5, day_of_week=0),  # 5 AM UTC = 12 AM EST
+    },
+}
